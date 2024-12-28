9f87dbcd46f2281187abba152ac7ee00e6dd1376
Luis Felipe Sanchez Sanchez <155125471+FelipeSanchezS@users.noreply.github.com>
2024-12-28 02:06:42 +0000

    agragamos comentariosde guia

--- ConsumoAPI/ConApi.py
+++ ConsumoAPI/ConApi.py
@@ -11,25 +11,27 @@ respuesta = requests.get(URL + pokemon)
 #Ingresamos en que formato queremos que nos nuestre los datos
 datos = respuesta.json()
 
+#Creamos el archivo .xtx
 with open(f"{pokemon}.txt", "w", encoding="utf-8") as archivo:
     print(f"--------- Los movimientos de {pokemon} son: ---------")
-    archivo.write(f"--------- Los movimientos de {pokemon} son: --------- \n")
+    archivo.write(f"--------- Los movimientos de {pokemon} son: --------- \n")#titulo de sección de las habilidades en el archivo txt
     for move in datos["moves"]:
         print(move["move"]["name"])
-        archivo.write(move["move"]["name"] + "\n")
+        archivo.write(move["move"]["name"] + "\n") #Tomamos la ruta de donde esta el dato que queremos capturatr la información
         
 
     print(f"--------- el tipo de ataque pokemon con nombre {pokemon} es: ---------")
-    archivo.write(f"--------- el tipo de ataque pokemon con nombre {pokemon} es: --------- \n")
+    archivo.write(f"--------- el tipo de ataque pokemon con nombre {pokemon} es: --------- \n")#titulo de sección de las habilidades en el archivo txt
     for type in datos["types"]:
         print(type["type"]["name"])
-        archivo.write(type["type"]["name"] + "\n")
+        archivo.write(type["type"]["name"] + "\n")#Tomamos la ruta de donde esta el dato que queremos capturatr la información
 
 
     print(f"--------- Las estadisticas del pokemon {pokemon} son: ---------")
-    archivo.write(f"--------- Las estadisticas del pokemon {pokemon} son: --------- \n")
+    archivo.write(f"--------- Las estadisticas del pokemon {pokemon} son: --------- \n")#titulo de sección de las habilidades en el archivo txt
     for stat in datos["stats"]:
         print(stat["stat"]["name"])
-        archivo.write(f"{stat['stat']['name']}: {stat['base_stat']}\n")
+        archivo.write(f"{stat['stat']['name']}: {stat['base_stat']}\n")#Tomamos la ruta de donde esta el dato que queremos capturatr la información
 
+#Mostramos y descargamos el archivo con la información ingresada y capturada
 print(f"La información del Pokémon {pokemon.capitalize()} se ha guardado en el archivo '{pokemon}.txt'.")
