638f9d6cd12f4944e84c06a451e03ca8b19dc866
Luis Felipe Sanchez Sanchez <155125471+FelipeSanchezS@users.noreply.github.com>
2024-12-28 02:03:24 +0000

    v2 donde permite exportar ciertos datos elegidos "Es posible ser modificados" a un .txt y poder ser visualizados

--- ConsumoAPI/ConApi.py
+++ ConsumoAPI/ConApi.py
@@ -11,10 +11,25 @@ respuesta = requests.get(URL + pokemon)
 #Ingresamos en que formato queremos que nos nuestre los datos
 datos = respuesta.json()
 
-print(f"--------- Los movimientos de {pokemon} son: ---------")
-for move in datos["moves"]:
-    print(move["move"]["name"])
+with open(f"{pokemon}.txt", "w", encoding="utf-8") as archivo:
+    print(f"--------- Los movimientos de {pokemon} son: ---------")
+    archivo.write(f"--------- Los movimientos de {pokemon} son: --------- \n")
+    for move in datos["moves"]:
+        print(move["move"]["name"])
+        archivo.write(move["move"]["name"] + "\n")
+        
 
-print(f"--------- el tipo de ataque pokemon con nombre {pokemon} es: ---------")
-for type in datos["types"]:
-    print(type["type"]["name"])
+    print(f"--------- el tipo de ataque pokemon con nombre {pokemon} es: ---------")
+    archivo.write(f"--------- el tipo de ataque pokemon con nombre {pokemon} es: --------- \n")
+    for type in datos["types"]:
+        print(type["type"]["name"])
+        archivo.write(type["type"]["name"] + "\n")
+
+
+    print(f"--------- Las estadisticas del pokemon {pokemon} son: ---------")
+    archivo.write(f"--------- Las estadisticas del pokemon {pokemon} son: --------- \n")
+    for stat in datos["stats"]:
+        print(stat["stat"]["name"])
+        archivo.write(f"{stat['stat']['name']}: {stat['base_stat']}\n")
+
+print(f"La información del Pokémon {pokemon.capitalize()} se ha guardado en el archivo '{pokemon}.txt'.")
