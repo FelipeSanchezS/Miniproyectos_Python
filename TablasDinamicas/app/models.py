5f90896572b7fd33f9ed2a8b3d597b3136353fc9
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-02-07 14:05:30 -0500

    Se crea la estructurá del proyecto ocn entorno virtual

---
+++ TablasDinamicas/app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Programmer(models.Model):
+    name=models.CharField(max_length=50)
+    country=models.CharField(max_length=3)
+    birthday=models.DateField()
