49a3f21303e89ff0ed7e1bc034bc3be0b140fa46
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-02-10 13:04:55 -0500

    creamos las diferentes ventanas

--- Crud_App_Web/App.py
+++ Crud_App_Web/App.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def Index():
     return 'Hello World'
 
+@app.route('/añadir')
+def añadir_contacto():
+    return 'Sección de añadir contacto'
+
+@app.route('/editar')
+def editar_contacto():
+    return 'Sección editar contacto'
+
+@app.route('/eliminar')
+def eliminar_contacto():
+    return 'Sección eliminar contacto'
+
 if __name__ == '__main__':
     app.run(port=3000, debug = True)
