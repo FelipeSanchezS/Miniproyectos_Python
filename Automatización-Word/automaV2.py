f79b0e26bd0ff2fce30b1b7fd65b2c60b6acb2f6
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-02-26 13:35:55 -0500

    Se crean diferentes párrafos y asi mismo se agregan párrafos

--- Automatización-Word/automaV2.py
+++ Automatización-Word/automaV2.py
@@ -5,26 +5,38 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH
 #Creamos el documento
 document = Document()
 
-#Primer Párrafo - va estar centrado con diferentes estilos en el mismo párrafo
-p1 = document.add_paragraph()
-p2 = document.add_paragraph()
+#En esta parte creamos los diferentes párrafos que vayamos a necesitar
+párrafo1 = document.add_paragraph()
+párrafo2 = document.add_paragraph()
+párrafo3 = document.add_paragraph()
 
 #Con esto centramos el párrafo
-p1.alignment = WD_ALIGN_PARAGRAPH.CENTER
-p2.alignment = WD_ALIGN_PARAGRAPH.LEFT
+párrafo1.alignment = WD_ALIGN_PARAGRAPH.CENTER
+párrafo2.alignment = WD_ALIGN_PARAGRAPH.LEFT
+párrafo3.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
 
 #Agregamos primer texto al párrafo y definimos otros estilos
-run1 = p1.add_run("Este es un texto") 
+run1 = párrafo1.add_run("Este es un texto") 
 run1.font.name = 'Arial' #Definimos letra
 run1.font.size = Pt(12) #Definimos tamaño
 
-#Agregamos Segunda texto al párrafo y definimos otros estilos
-run2 = p2.add_run("Con otro tipo de letra se diseño la segunda linea hacia la izquierda") 
+#Agregamos Segundo párrafo y definimos otros estilos
+run2 = párrafo2.add_run("Con otro tipo de letra se diseño la segunda linea hacia la izquierda - ") 
 run2.font.name = 'Times New Roman' #Definimos letra
 run2.font.size = Pt(14) #Definimos tamaño
 run2.font.bold = True
 
+#Agregamos mas texto en el párrafo 2 y definimos otros estilos
+run3 = párrafo2.add_run("Es la continuación de la misma segunda linea con otros estilos para seguir probando") 
+run3.font.name = 'Calibri' #Definimos letra
+run3.font.size = Pt(18) #Definimos tamaño
+run3.font.bold = True
 
+#Agregamos párrafo 3 y agregamos textos
+run4 = párrafo3.add_run("Este es el estilo correspondiente al tercer párrafo del documento que se esta creando") 
+run4.font.name = 'Georgia' #Definimos letra
+run4.font.size = Pt(11) #Definimos tamaño
+run4.font.color.rgb = RGBColor(0,0,255)
 
 #Guardamos el documento
 document.save("AutomatizaciónV2.docx")
