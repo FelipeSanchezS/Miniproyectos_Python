d955341a585c06666392bbafca4eb974914db22b
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-02-26 13:47:41 -0500

    Se reestructura para entender de mejor manera

--- Automatización-Word/automaV2.py
+++ Automatización-Word/automaV2.py
@@ -11,18 +11,24 @@ párrafo2 = document.add_paragraph()
 párrafo3 = document.add_paragraph()
 párrafo4 = document.add_paragraph()
 
-#Con esto centramos el párrafo
+
+#------PÁRRAFO 1-------
+#En esta parte creamos los diferentes párrafos que vayamos a necesitar
+párrafo1 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
 párrafo1.alignment = WD_ALIGN_PARAGRAPH.CENTER
-párrafo2.alignment = WD_ALIGN_PARAGRAPH.LEFT
-párrafo3.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-párrafo4.alignment = WD_ALIGN_PARAGRAPH.RIGHT
-
-
 #Agregamos primer texto al párrafo y definimos otros estilos
 run = párrafo1.add_run("Este es un texto.") 
 run.font.name = 'Arial' #Definimos letra
 run.font.size = Pt(12) #Definimos tamaño
 
+
+
+#------PÁRRAFO 2-------
+#En esta parte creamos los diferentes párrafos que vayamos a necesitar
+párrafo2 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo2.alignment = WD_ALIGN_PARAGRAPH.LEFT
 #Agregamos Segundo párrafo y definimos otros estilos
 run = párrafo2.add_run("Con otro tipo de letra se diseño la segunda linea hacia la izquierda - ") 
 run.font.name = 'Times New Roman' #Definimos letra
@@ -33,12 +39,26 @@ run.font.name = 'Calibri' #Definimos letra
 run.font.size = Pt(18) #Definimos tamaño
 run.font.bold = True
 
+
+
+#------PÁRRAFO 3-------
+#En esta parte creamos los diferentes párrafos que vayamos a necesitar
+párrafo3 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo4.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo3.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
 #Agregamos párrafo 3 y agregamos textos
 run = párrafo3.add_run("Este es el estilo correspondiente al tercer párrafo del documento que se esta creando.") 
 run.font.name = 'Georgia' #Definimos letra
 run.font.size = Pt(11) #Definimos tamaño
 run.font.color.rgb = RGBColor(0,0,255)
 
+
+
+#------PÁRRAFO 4-------
+#En esta parte creamos los diferentes párrafos que vayamos a necesitar
+párrafo4 = document.add_paragraph()
 #Agregamos párrafo 4 y agregamos textos
 run = párrafo4.add_run("Este es el siguiente párrafo, (párrafo 4) creado con los diferentes estilos ya predefinidos") 
 run.font.name = 'Verdana' #Definimos letra
