aa1896b08bf6ee1c90e7fd450096b149f92f7d6c
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-03-07 19:50:41 -0500

    Se importan librerias

--- Auto-Doc-Renuncia/main.py
+++ Auto-Doc-Renuncia/main.py
@@ -1,1 +1,10 @@
-print('aca va la automatización de documentos')
+#Importamos librerias
+from docx import Document
+from docx.shared import Pt, RGBColor
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+
+
+
+#Creamos el documento
+document = Document()
