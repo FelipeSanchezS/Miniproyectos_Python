9945a27867426b2b527c91c2680f19799022df43
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-03-07 20:16:30 -0500

    Se agrega parte inicial del archivo

--- Auto-Doc-Renuncia/main.py
+++ Auto-Doc-Renuncia/main.py
@@ -3,8 +3,106 @@ from docx import Document
 from docx.shared import Pt, RGBColor
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
-
-
-
 #Creamos el documento
 document = Document()
+
+#Ingresamos los datos necesarios
+nombre = input('Ingrese su nombre: ')
+dirección = input('Ingrese su dirección de residencia: ')
+email = input('Ingrese su correo: ')
+teléfono = input('Ingrese su teléfono: ')
+fechaA = input('Ingrese la fecha: ')
+empresa = input('Ingrese nombre de la empresa: ')
+ciudadPaís = input('Ingrese ciudad y país: ')
+
+
+#------Primera linea-------
+párrafo1 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo1.alignment = WD_ALIGN_PARAGRAPH.LEFT
+#Agregamos párrafo 4 y agregamos textos
+run = párrafo1.add_run(nombre) 
+run.font.name = 'Courier New' #Definimos letra
+run.font.size = Pt(13) #Definimos tamaño
+run.font.italic = True
+
+#------Segunda linea-------
+párrafo2 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo2.alignment = WD_ALIGN_PARAGRAPH.LEFT
+#Agregamos párrafo 4 y agregamos textos
+run = párrafo2.add_run(dirección) 
+run.font.name = 'Courier New' #Definimos letra
+run.font.size = Pt(13) #Definimos tamaño
+run.font.italic = True
+
+#------Tercera linea-------
+párrafo3 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo3.alignment = WD_ALIGN_PARAGRAPH.LEFT
+#Agregamos párrafo 4 y agregamos textos
+run = párrafo3.add_run(email) 
+run.font.name = 'Courier New' #Definimos letra
+run.font.size = Pt(13) #Definimos tamaño
+run.font.italic = True
+
+#------Cuarta linea-------
+párrafo4 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo4.alignment = WD_ALIGN_PARAGRAPH.LEFT
+#Agregamos párrafo 4 y agregamos textos
+run = párrafo4.add_run(teléfono) 
+run.font.name = 'Courier New' #Definimos letra
+run.font.size = Pt(13) #Definimos tamaño
+run.font.italic = True
+
+#------Quinta linea-------
+párrafo5 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo5.alignment = WD_ALIGN_PARAGRAPH.LEFT
+#Agregamos párrafo 4 y agregamos textos
+run = párrafo5.add_run(fechaA) 
+run.font.name = 'Courier New' #Definimos letra
+run.font.size = Pt(13) #Definimos tamaño
+run.font.italic = True
+
+#------Salto de linea-------
+document.add_paragraph()
+
+#------Sexta linea-------
+párrafo6 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo6.alignment = WD_ALIGN_PARAGRAPH.LEFT
+#Agregamos párrafo 4 y agregamos textos
+run = párrafo6.add_run(empresa) 
+run.font.name = 'Courier New' #Definimos letra
+run.font.size = Pt(13) #Definimos tamaño
+run.font.italic = True
+
+#------Séptima linea-------
+párrafo7 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo7.alignment = WD_ALIGN_PARAGRAPH.LEFT
+#Agregamos párrafo 4 y agregamos textos
+run = párrafo7.add_run(ciudadPaís) 
+run.font.name = 'Courier New' #Definimos letra
+run.font.size = Pt(13) #Definimos tamaño
+run.font.italic = True
+
+#------Salto de linea-------
+document.add_paragraph()
+
+#------Octava linea-------
+párrafo7 = document.add_paragraph()
+#Con esto elegimos dirección del párrafo el párrafo
+párrafo7.alignment = WD_ALIGN_PARAGRAPH.LEFT
+#Agregamos párrafo 4 y agregamos textos
+run = párrafo7.add_run("Estimado/a "+empresa+": ") 
+run.font.name = 'Courier New' #Definimos letra
+run.font.size = Pt(13) #Definimos tamaño
+run.font.italic = True
+
+
+
+#Guardamos el documento
+document.save("Carta_Renuncia "+nombre+".docx")
