d23ffb27bcb83b28e49d98a0e0529026799a61a5
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-03-14 16:52:27 -0500

    Se inicia con la conversión

---
+++ Conversor-Img/main.py
@@ -0,0 +1,10 @@
+#Importamos librerías
+import os
+from PIL import Image
+
+def listar_formatos_soportados():
+    """Muestra los formatos de imagen soportados"""
+    formatos = ["JPG", "JPEG", "PNG", "GIF", "BMP", "TIFF", "WEBP"]
+    print("Formatos soportados")
+    for formato in formatos:
+        print(f"-{formato}")
