110291646f78008480e2791187e53626aebab331
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-03-19 12:21:37 -0500

    definimos los campos y la función

---
+++ Gen-Datos-Falsos/main.py
@@ -0,0 +1,20 @@
+#Importamos librerías
+import streamlit
+from faker import fake
+import pandas as pd
+
+fake = Faker('es_CL')
+
+available_fields = {
+    'Nombre' : fake.name,
+    'Dirección' : fake.address,
+    'Correo electrónico' : fake.email,
+    'Teléfono' : fake.phone_number,
+    'Empresa' : fake.company,
+    'Fecha' : fake.date,
+    'Número de tarjeta de crédito' : fake.credit_card_number
+}
+
+def generate_fake_data(fields, num_rows):
+    data = {field: [func() for _ in range(num_rows)] for field, func in fields.items()}
+    return pd.Dataframe(data)
