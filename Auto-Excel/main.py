eb79ca3935fadd6782c625ab42c847107ca65615
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-04-07 12:41:01 -0500

    creamos un dataset de prueba

--- Auto-Excel/main.py
+++ Auto-Excel/main.py
@@ -3,3 +3,12 @@ import os
 import pandas as pd
 import seaborn
 import mathplotlib.pyplot as plt
+
+#Creamos un dataset de ejemplo
+datos = {
+    'Nombre': ['Juan', 'Ana', 'Pedro', 'Maria'],
+    'Edad': [1000, 2000, 3000, 4000, 5000],
+    'Ciudad': [2, 4, 6, 8, 10],}
+
+df = pd.DataFrame(datos)
+print(df)
