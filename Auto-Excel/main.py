d8a662a7a6f9ab57a576e5dfc700bcfc7fa9c2ad
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-04-07 12:45:43 -0500

    Se imprime el dataset

--- Auto-Excel/main.py
+++ Auto-Excel/main.py
@@ -2,13 +2,13 @@ import openpyxl
 import os
 import pandas as pd
 import seaborn
-import mathplotlib.pyplot as plt
+
 
 #Creamos un dataset de ejemplo
 datos = {
-    'Nombre': ['Juan', 'Ana', 'Pedro', 'Maria'],
-    'Edad': [1000, 2000, 3000, 4000, 5000],
-    'Ciudad': [2, 4, 6, 8, 10],}
+    'Producto': ['Juan', 'Ana', 'Pedro', 'Maria', 'Luis'],
+    'Precio': [1000, 2000, 3000, 4000, 5000],
+    'Cantidad': [2, 4, 6, 8, 10]}
 
 df = pd.DataFrame(datos)
 print(df)
