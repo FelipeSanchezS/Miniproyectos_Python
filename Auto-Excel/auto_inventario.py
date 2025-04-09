dd4bf1641bec7c5295380d3b413404798a7cb46f
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-04-09 12:42:08 -0500

    Se crea y inicia con formato de automatización

--- Auto-Excel/auto_inventario.py
+++ Auto-Excel/auto_inventario.py
@@ -7,8 +7,23 @@ def cargar_inventario():
     try:
         # Cargar el archivo de Excel existente
         wb = openpyxl.load_workbook('Inventario.xlsx')
-        ws = wb.active
-        return wb, ws
+        #return wb, ws
     except FileNotFoundError:
         print("El archivo 'Inventario.xlsx' no existe. Creando uno nuevo.")
         return None
+    
+    ws = wb.active
+    ws.title = 'Inventario principal'
+
+    #Aplicamos formato a los encabezados
+    for cell in ws[1]:
+        cell.font = Font(bold=True, color="FFFFFF")
+        cell.fill = PatternFill(start_color="0000FF", end_color="0000FF", fill_type="solid")
+        cell.alignment = Alignment(horizontal='center', vertical='center')
+        cell.border = openpyxl.styles.Border(
+            left=openpyxl.styles.Side(style='thin', color='000000'),
+            right=openpyxl.styles.Side(style='thin', color='000000'),
+            top=openpyxl.styles.Side(style='thin', color='000000'),
+            bottom=openpyxl.styles.Side(style='thin', color='000000')
+        )
+    return wb, ws
