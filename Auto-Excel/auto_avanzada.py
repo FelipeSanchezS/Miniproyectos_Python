1a6651328a8dc16962c0a2676a2a3f9201f50d46
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-04-15 12:38:47 -0500

    Importamos librerias

--- Auto-Excel/auto_avanzada.py
+++ Auto-Excel/auto_avanzada.py
@@ -1,1 +1,11 @@
 #Sección de gráficos
+#Importar librerías
+import openpyxl
+from openpyxl.styles import Font, Color, PatternFill, Border, Side
+from openpyxl.chart import BarChart, Reference, LineChart, PieChart, ScatterChart
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.drawing.fill import GradientFill
+from openpyxl.worksheet.datavalidation import DataValidation
+import datetime
+import os
+import win32com.client as win32
